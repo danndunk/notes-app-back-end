6c9dee1cf9cdadef54dc26a434d62fddf3469c18
2299766808 <dandunk.ariyon@bankmandiri.co.id>
2023-06-22 14:55:38 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,15 +4,15 @@ const routes = require("./routes");
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: "localhost",
-    routes:{
-        cors : {
-            origin : ["*"]
-        }
-    }
+    host: process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
   });
 
-  server.route(routes)
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
